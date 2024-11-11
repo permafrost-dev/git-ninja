53aa90668e6da6911b992bcdbdddd869c78bc83f
Patrick Organ <patrick@permafrost.dev>
2024-11-11 17:51:33 -0500

    add --switch flag to branch:last to checkout the branch

--- cmd/branchLast.go
+++ cmd/branchLast.go
@@ -7,13 +7,35 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var flagCheckout bool = false
+
+func switchToBranch(branchName string) {
+	err := helpers.RunCommandOnStdout("git", "checkout", branchName)
+	if err != nil {
+		fmt.Println("Error switching branches:", err)
+		return
+	}
+
+	fmt.Printf("Switched to branch '%s'\n", branchName)
+}
+
+var branchLastCmd = &cobra.Command{
+	Use:   "branch:last",
+	Short: "Work with the last checked out branch",
+	Run: func(cmd *cobra.Command, args []string) {
+		branchName, _ := helpers.GetLastCheckedoutBranchName()
+
+		if flagCheckout {
+			switchToBranch(branchName)
+			return
+		}
+
+		fmt.Println(branchName)
+	},
+}
+
 func init() {
-	rootCmd.AddCommand(&cobra.Command{
-		Use:   "branch:last",
-		Short: "Show the last checked out branch name",
-		Run: func(cmd *cobra.Command, args []string) {
-			branchName, _ := helpers.GetLastCheckedoutBranchName()
-			fmt.Println(branchName)
-		},
-	})
+	rootCmd.AddCommand(branchLastCmd)
+
+	branchLastCmd.Flags().BoolVarP(&flagCheckout, "switch", "s", false, "Switch to the last checked out branch")
 }
